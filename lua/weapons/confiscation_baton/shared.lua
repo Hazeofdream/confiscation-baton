a1697c68be2a8fca6b9f9b1f962667f18f7b6359
Haze_of_dream <hazeofdreamx@gmail.com>
2026-06-09 01:07:43 -0500

    3 mil on 9 hours is a nah

--- lua/weapons/confiscation_baton/shared.lua
+++ lua/weapons/confiscation_baton/shared.lua
@@ -25,7 +25,7 @@ function loadContraband()
 		-- 2) The longer an entity exist, the more likelihood of the base being extremely tough to crack, so we need to make sure that it's worth the risk of cracking a tough base, and
 		-- 3) We dont want to make the time scale too harshly towards the end or it encourages police to wait intentionally, and encouraging non-interaction is detrimental to roleplay.
 		["time_bonus_interval"] = 20,
-		["time_bonus_amount"] = 2200
+		["time_bonus_amount"] = 5000
 	}
 
 	-- no servers run these but it DOES exist
